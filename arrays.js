7a27d98966aebb6903197c9c7ec2096476a4a89b
Fernando Albar <falbarp@users.noreply.github.com>
2022-11-15 12:29:57 +0100

    Update arrays.js

--- arrays.js
+++ arrays.js
@@ -9,3 +9,14 @@ console.log(deduped)
 
 const deduped2 = [...new Set(people)]
 console.log(deduped2)
+
+
+/* Returns a range with params stop number, start number and step numbrer */
+function range(stop, start = 1, step = 1) {
+       return Array.from(
+               { length: (stop - start) / step + 1 },
+               (_, i) => start + i * step
+       );
+}
+
+// range (5) returns [1,2,3,4,5]
