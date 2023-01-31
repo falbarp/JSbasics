42840a026b6964884d3ca88b1e18c51289b2c88c
Fernando Albar <falbarp@users.noreply.github.com>
2023-01-31 09:52:09 +0100

    Update arrays.js

--- arrays.js
+++ arrays.js
@@ -12,11 +12,10 @@ console.log(deduped2)
 
 
 /* Returns a range with params stop number, start number and step numbrer */
-function range(stop, start = 1, step = 1) {
-       return Array.from(
-               { length: (stop - start) / step + 1 },
-               (_, i) => start + i * step
-       );
-}
 
-// range (5) returns [1,2,3,4,5]
+const range = (start, stop, step=1) => {
+  return Array.from(
+    { length: (stop - start) / step + 1 },
+    (value, i) => start + i * step
+  )
+}
